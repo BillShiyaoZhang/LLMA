6639a9216e23199b52405e2e289daf2a89dc52e2
Shiyao Zhang <zhangshiyao_ZSY@outlook.com>
2025-07-01 13:40:56 +0800

    Update EvaluateMatcher.java

--- LLMAMatcher/src/test/java/cn/edu/xjtlu/iot/syzhang/LLMA/EvaluateMatcher.java
+++ LLMAMatcher/src/test/java/cn/edu/xjtlu/iot/syzhang/LLMA/EvaluateMatcher.java
@@ -14,25 +14,12 @@ import org.junit.jupiter.api.Assertions;
 public class EvaluateMatcher {
     
     @Test
-    public void evalSimpleMatcher(){        
-        ExecutionResultSet result = Executor.run(TrackRepository.Anatomy.Default, new SimpleStringMatcher());
+    public void evalLLMAMatcherMatcher(){
+//        ExecutionResultSet result = Executor.run(TrackRepository.Anatomy.Default, new SimpleStringMatcher());
+        ExecutionResultSet result = Executor.run(TrackRepository.Anatomy.Default, new LLMAMatcher());
         ExecutionResult r = result.iterator().next();
         System.out.print(r.getSystemAlignment());
     }
     
-    @Test
-    public void evalResourceMatcher(){        
-        ExecutionResultSet result = Executor.run(TrackRepository.Anatomy.Default, new LoadResourceMatcher());
-    }
-    
-    @Test
-    public void optimizeMatcher(){
-        GridSearch gridSearch = new GridSearch(LevenshteinMatcher.class);
-        gridSearch.addParameter("threshold", Arrays.asList(0.1, 0.3, 0.5, 0.7, 1.0));
-        ExecutionResultSet executionResultSet = gridSearch.runGridSequential(TrackRepository.Conference.V1.getTestCase("cmt-conference"));
-        Assertions.assertTrue(executionResultSet.size() > 0);
-        EvaluatorCSV evaluator = new EvaluatorCSV(executionResultSet);
-        evaluator.writeToDirectory();
-    }
-    
+
 }
